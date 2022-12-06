23d420c0673a9df3a3bd6a71e195d4c829bca10f
slandymani <slandyman9@gmail.com>
2022-12-06 12:29:51 +0200

    add amount; add generated contracts

---
+++ internal/proxy/evm/utils.go
@@ -0,0 +1,63 @@
+package evm
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Swapica/swapica-svc/resources"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"math/big"
+)
+
+const gasLimit = 300000
+
+func skipSig(address common.Address, transaction *types.Transaction) (*types.Transaction, error) {
+	return transaction, nil
+}
+
+func buildTransactOpts(from common.Address) *bind.TransactOpts {
+	return buildTransactOptsWithValue(from, nil)
+}
+
+func buildTransactOptsWithValue(from common.Address, value *big.Int) *bind.TransactOpts {
+	return &bind.TransactOpts{
+		From:     from,
+		Signer:   skipSig,
+		NoSend:   true,
+		GasLimit: gasLimit,
+		Value:    value,
+	}
+}
+
+func parseChainParams(params json.RawMessage) (resources.EvmChainParams, error) {
+	chainParams := resources.EvmChainParams{}
+	err := json.Unmarshal(params, &chainParams)
+	return chainParams, err
+}
+
+func encodeTx(tx *types.Transaction, from common.Address, chainID *big.Int, chain string, confirmed *bool) (interface{}, error) {
+	return resources.EvmTransaction{
+		Key: resources.Key{
+			ID:   tx.Hash().String(),
+			Type: resources.EVM_TRANSACTION,
+		},
+		Attributes: resources.EvmTransactionAttributes{
+			Confirmed: confirmed,
+			TxBody: resources.EvmTransactionTxBody{
+				From:    from.String(),
+				To:      tx.To().String(),
+				Value:   tx.Value().String(),
+				Data:    hexutil.Encode(tx.Data()),
+				ChainId: fmt.Sprintf("0x%x", chainID.Int64()),
+			},
+		},
+		Relationships: resources.EvmTransactionRelationships{
+			Chain: resources.Key{
+				ID:   chain,
+				Type: resources.CHAIN,
+			}.AsRelation(),
+		},
+	}, nil
+}
