926d36b2ea48ab461fea1ff9042d38ef46d5464c
violog <51th.apprent1ce.f0rce@gmail.com>
2023-03-01 16:13:10 +0200

    Stringify amount for FE

--- internal/service/handlers/create_order.go
+++ internal/service/handlers/create_order.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"errors"
+	"math/big"
 	"net/http"
 
 	"github.com/Swapica/swapica-svc/internal/proxy/types"
@@ -9,6 +10,7 @@ import (
 	"github.com/Swapica/swapica-svc/internal/service/requests"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
+	"gitlab.com/distributed_lab/logan/v3"
 )
 
 func CreateOrder(w http.ResponseWriter, r *http.Request) {
@@ -45,13 +47,22 @@ func CreateOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	amountToBuy,ok1:=big.NewInt(0).SetString(request.AmountToBuy,10)
+	amountToSell,ok2:=big.NewInt(0).SetString(request.AmountToSell,10)
+	if !ok1||!ok2{
+		msg:="amount_to_buy or amount_to_sell is not big integer"
+		Log(r).WithFields(logan.F{"amount_to_buy": request.AmountToBuy,"amount_to_sell": request.AmountToSell}).Debug(msg)
+		ape.RenderErr(w, problems.BadRequest(nil)...)
+		return
+	}
+
 	tx, err := ProxyRepo(r).Get(request.SrcChain).CreateOrder(types.CreateOrderParams{
 		Sender:       request.Sender,
 		SrcChain:     *srcChain,
 		TokenToSell:  request.TokenToSell,
-		AmountToSell: request.AmountToSell,
+		AmountToSell: amountToSell,
 		TokenToBuy:   request.TokenToBuy,
-		AmountToBuy:  request.AmountToBuy,
+		AmountToBuy:  amountToBuy,
 		DestChain:    *destChain,
 	})
 	if err != nil {
